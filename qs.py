6d65f9125e3dbc2963b2cc43045f5aaef44be9ef
coleman-jarvin <colemanjarvin13@gmail.com>
2026-07-28 17:49:07 -0500

    what am i doing

--- qs.py
+++ qs.py
@@ -1,7 +1,6 @@
 ## Quantum States 
 
 ### Superposition & Measurement
-
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -9,23 +8,39 @@ import matplotlib.pyplot as plt
 
 class System: 
     
-    def __init__(self, spin, _appa, vector):
+    def __init__(self, spin: int , _appa: float, vector: list[float] ):
         self.spin = spin 
         self.appa = _appa
         self.vector = vector
 
-    def normalization(self):
+    def normalization(self, _appa, vector: list[int]):
         ## set degree orientation of the apparatus
         ## _appa = np.degrees(90)
         ### convert into radians
-        pass
+        
+        _appa = 0.5
+
+        for v in vector:
+            spin = 0
+            if v in vector <= 0: 
+                v = vector[v] * _appa
+            else: 
+                v = 0.5
+
+            while v:
+                if v == 1:
+                    spin = np.radians(180) * _appa
+            return spin 
+
+        print(spin) 
+                
 
     def measure(self, two_state, state_1, state_2, bias):
 
         H = [1,0] 
         T = [0,1]
         state_1 = H 
-        #state_2 = T
+        state_2 = T
 
         two_state = (state_1, state_2)
 
@@ -69,7 +84,12 @@ class System:
 
 
 if __name__ == "__main__":
-    State(any)
+    System(spin = 1, _appa = 0.5, vector=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2])
+    
+    _appa = 0.5
+    _appa.normalization(_appa=0.5)
 
-    pass
+
+
+     
 
